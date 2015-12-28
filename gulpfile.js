179d8c3a6cd2d100717336f891c7921d4562fe81
yangchaoben <842293890@qq.com>
2015-12-28 12:44:43 +0800

    更改目录结构，增加gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/**
+ * 组件安装
+ * npm install gulp-util gulp-imagemin gulp-ruby-sass gulp-minify-css gulp-jshint gulp-uglify gulp-rename gulp-concat gulp-clean gulp-livereload tiny-lr --save-dev
+ */
+
+// 引入 gulp及组件
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+   //concat = require('gulp-concat'),
+   //  imagemin = require('gulp-imagemin'),
+
+
+gulp.task('watch', function() {
+    gulp.watch("src/canvas" + '**/*.*',['reload-dev']);
+});
+
+gulp.task('connectDev', function() {
+  connect.server({
+    root: "src/canvas",
+    port: 8000,
+    livereload: true
+  });
+});
+
+//reload server
+gulp.task('reload-dev',function() {
+  gulp.src(path.src + '**/*.*')
+    .pipe(connect.reload());
+});
+//测试服务器
+gulp.task('default', ['connectDev', 'watch']);
