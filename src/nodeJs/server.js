fe43150538cca2338eeb62e4865e6a46d0d6ca4a
sheepshine <842293890@qq.com>
2016-06-13 14:14:44 +0800

    post请求

--- src/nodeJs/server.js
+++ src/nodeJs/server.js
@@ -3,9 +3,23 @@ var url =require("url");
 
 function start(route,handle){
 	function onRequest(request, response){ 
+		// var pathname=url.parse(request.url).pathname;
+		// console.log(pathname,111)
+		// route(handle,pathname,response);
+
+		var postData='';
 		var pathname=url.parse(request.url).pathname;
-		console.log(pathname,111)
-		route(handle,pathname,response);
+
+		request.setEncoding('utf-8');
+
+		request.addListener('data',function(postDataChunk){
+			postData+=postDataChunk;
+			console.log(postDataChunk);
+		})
+
+		request.addListener('end',function(){
+			route(handle,pathname,response,postData)
+		})
 	} 
 
 	http.createServer(onRequest).listen(8888); 
